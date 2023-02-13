a6d618097b7c25734f2381de76dc679c8b0d807c
Daniel Wierbicki <daniel@wierbicki.org>
2023-02-13 13:26:11 +0100

    Datei mit übung online gestellt. CREATE und INSERT.

---
+++ 2023-02-13-sql/db2.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE db2;
+
+CREATE TABLE db2.user (
+    name VARCHAR(25) NOT NULL,
+    password VARCHAR(20) NOT NULL
+);
+
+INSERT INTO db2.user
+(name, password)
+VALUES 
+("otto", "asdf1234"), 
+("hans", "geheim");
+
+INSERT INTO db2.user 
+(name, password)
+VALUES 
+("peter", "123456"), 
+(" daniel", "geheimesPW"), 
+(" fabian", "abcd");
